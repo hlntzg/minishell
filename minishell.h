5ebea348acb7a7eb6ce57b7263eccd17b7acf72b
Natalie Meintjes <nmeintje@c1r6p7.hive.fi>
2024-12-02 16:45:37 +0200

    added some stuff about ASTs

--- minishell.h
+++ minishell.h
@@ -42,6 +42,73 @@ typedef struct s_tok
 	t_tok	*next;
 }	t_tok;
 
+/*AST example 1*/
+typedef struct Parser
+{
+	enum { WORD, PIPE, REDIN, REDOUT, APPEND, HEREDOC, ENVMT, 
+			BUILTIN} type;
+	union {
+		// lots of specific things for each type of token
+		int		integerExp;
+        char	*stringExp;
+        char	*variableExp;
+        struct { char	*operator;
+                   struct Exp*      left;
+                   struct Exp*      right; }       binaryExp;
+        struct { char	*operator;
+                   struct Exp*      operand; }     unaryExp;
+          struct { char	*name;
+                   struct Exp_list* arguments; }   callExp;
+          struct { struct Exp*  record;
+                   char	*attribute; }       projectionExp;
+          struct rec { char	*attribute;
+                       struct Exp*  value;
+                       struct rec*  next; }        recordExp;
+	} tokens;
+} ast;
+
+//Exp_list: a list of ASTs;
+typedef struct Exp_list { 
+  ast*             elem;
+  struct Exp_list* next;
+} ast_list;
+
+/* It's a good idea to define a constructor for every kind of 
+expression to simplify the task of constructing ASTs:
+ast* make_binaryExp ( string operator, ast* left, ast* right ) {
+  ast* e = (ast*) malloc(sizeof(ast));
+  e->tag = binary_exp;
+  e->op.binary.operator = operator;
+  e->op.binary.left = left;
+  e->op.binary.right = right;
+  return e;
+};
+For example,
+make_binaryExp("+",make_binaryExp("-",make_variableExp("x"),make_integerExp(2)),
+               make_integerExp(3))
+constructs the AST for the input (x-2)+3. */
+
+
+AST example 2
+Tagged union
+We define our AST type as a struct holding two members,
+
+an enum called tag, enumerating each node type, and
+a union called data, consisting of data members for each tag type.
+typedef struct AST AST; // Forward reference
+
+struct AST {
+  enum {
+    AST_NUMBER,
+    AST_ADD,
+    AST_MUL,
+  } tag;
+  union {
+    struct AST_NUMBER { int number; } AST_NUMBER;
+    struct AST_ADD { AST *left; AST *right; } AST_ADD;
+    struct AST_MUL { AST *left; AST *right; } AST_MUL;
+  } data;
+} ast; 
 
 //void	set_signals(void);
 
