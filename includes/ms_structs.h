d35fab25043e7fa15ae05108d22e5a66a55b960a
Helena Utzig <helenautzig@gmail.com>
2025-01-22 10:18:11 +0200

    norm

--- includes/ms_structs.h
+++ includes/ms_structs.h
@@ -1,26 +1,14 @@
 #ifndef MS_STRUCTS_H
 # define MS_STRUCTS_H
 
-typedef struct	s_env
+typedef struct s_env
 {
 	char			*key;
 	char			*value;
 	struct s_env	*next;
 	struct s_env	*prev;
 }	t_env;
-/*
-typedef struct	s_cmd
-{
-	char	*command;
-	char	**args;
-	int		infile;
-	int		outfile_t;
-	int		outfile_a;
-	int		heredoc;
-	int		tmp_doc[2];
-	char	*eof;
-}	t_cmd;
-*/
+
 typedef enum e_ast_status
 {
 	READY,
@@ -43,53 +31,45 @@ typedef enum e_type
 
 typedef enum e_quote_state
 {
-    NONE,
-    SINGLE,
-    DOUBLE
+	NONE,
+	SINGLE,
+	DOUBLE
 }	t_quote_state;
 
 typedef struct s_token
 {
-	t_type type;
-	char	*content;
+	t_type			type;
+	char			*content;
 	struct s_token	*next;
 }	t_token;
 
 typedef struct s_tree_node
 {
-  t_type  type;
-  char        **value;
-  int		status;
-  struct  s_tree_node *left;
-  struct  s_tree_node *right;
-} t_tree_node;
+	t_type				type;
+	char				**value;
+	int					status;
+	struct s_tree_node	*left;
+	struct s_tree_node	*right;
+}	t_tree_node;
 
-typedef struct  s_data
+typedef struct s_data
 {
-	t_env	*env;
-//	t_cmd	*cmd;
+	t_env		*env;
 	t_tree_node	*tree;
-    char    *prompt;
-	char	*cwd;
-	char	*input_user;
-	char	**envp;
-	char	**envp_path;
-	int		total_cmds; //maybe remove
-	int		total_process; // maybe remove
-	int		*pid;
-//	int		*fd;  //array of fd for pipes
-	int		exit_code;
-	int		count_infile;
-	int		count_outfile;
-	int		count_pipe;
-	int		processes;
-	int		count_child;
-//	int		executed_pipe;
-	int		redirect_input;
-	int		redirect_output;
-	int		fd[2];
-//	int		og_stdin;
-//	int		og_stdout;
-}   t_data;
+	char		*prompt;
+	char		*cwd;
+	char		*input_user;
+	char		**envp;
+	char		**envp_path;
+	int			exit_code;
+	int			count_infile;
+	int			count_outfile;
+	int			count_pipe;
+	int			processes;
+	int			count_child;
+	int			redirect_input;
+	int			redirect_output;
+	int			fd[2];
+}	t_data;
 
 #endif
--- ms_pre_exe_newline.c
+++ ms_pre_exe_newline.c
@@ -6,7 +6,7 @@ void	exe_get_total_redirections_and_pipes(t_data *data, t_tree_node *ast)
 		data->count_pipe += 1;
 	else if (ast->type == REDIN || ast->type == HEREDOC)
 		data->count_infile += 1;
-	else if (ast->type == REDOUT_A ||ast->type == REDOUT_T)
+	else if (ast->type == REDOUT_A || ast->type == REDOUT_T)
 		data->count_outfile += 1;
 	if (ast->left)
 		exe_get_total_redirections_and_pipes(data, ast->left);
@@ -14,8 +14,6 @@ void	exe_get_total_redirections_and_pipes(t_data *data, t_tree_node *ast)
 		exe_get_total_redirections_and_pipes(data, ast->right);
 	if (data->count_pipe)
 		data->processes = data->count_pipe + 1;
-//	else
-//		data->processes = 1;
 }
 
 int	ms_pre_exe_newline(t_data *data)
@@ -26,26 +24,15 @@ int	ms_pre_exe_newline(t_data *data)
 	data->envp_path = exe_get_path(data->envp);
 	if (!data->envp_path)
 		return (ms_error(ERR_MALLOC_FAIL, NULL, 1, FAILURE));
-	data->pid = malloc(sizeof(int) * data->total_cmds);
-	if (!data->pid)
-		return (ms_error(ERR_MALLOC_FAIL, NULL, 1, FAILURE));
-//	data->fd = malloc(sizeof(int) * (2 * (data->total_cmds - 1))); // total_cmds - 1 = total_process ??? -->  process is the pipe count 
-//	if (!data->fd)
-//		return (ms_error(ERR_MALLOC_FAIL, NULL, 1, FAILURE));
-	
 	data->count_infile = 0;
 	data->count_outfile = 0;
 	data->count_pipe = 0;
 	data->redirect_input = 0;
 	data->redirect_output = 0;
-//	data->executed_pipe = 0;
 	data->processes = 0;
 	data->count_child = 0;
 	data->fd[0] = STDIN_FILENO;
 	data->fd[1] = STDOUT_FILENO;
-//	data->og_stdin = dup(STDIN_FILENO);
-//	data->og_stdout = dup(STDOUT_FILENO);
-
 	exe_get_total_redirections_and_pipes(data, data->tree);
 	return (SUCCESS);
 }
