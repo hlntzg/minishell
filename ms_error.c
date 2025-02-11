357076440cc3c85967cf686954a8ad28576d3acf
Helena Utzig <helenautzig@gmail.com>
2025-02-11 09:26:03 +0200

    norm OK ms_error

--- ms_error.c
+++ ms_error.c
@@ -1,4 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 09:19:20 by hutzig            #+#    #+#             */
+/*   Updated: 2025/02/11 09:21:48 by hutzig           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./includes/ms.h"
+
+void	ms_errno_message(char *str, int errno_code)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putstr_fd(str, STDERR_FILENO);
+	if (errno_code == EISDIR)
+		ft_putendl_fd(ERR_IS_DIR, STDERR_FILENO);
+	else if (errno_code == EACCES)
+		ft_putendl_fd(ERR_PERMISSION, STDERR_FILENO);
+	else if (errno_code == ENOENT)
+		ft_putendl_fd(ERR_NO_FILE_OR_DIR, STDERR_FILENO);
+	else if (errno_code == ENOTDIR)
+		ft_putendl_fd(ERR_NOT_DIR, STDERR_FILENO);
+	else
+		ft_putendl_fd(strerror(errno_code), STDERR_FILENO);
+}
 
 int	ms_error(char *str, char *msg, int err_code, int return_code)
 {
@@ -10,51 +38,15 @@ int	ms_error(char *str, char *msg, int err_code, int return_code)
 	}
 	else if (errno)
 	{
-//		printf("errno = %d\n", errno);
-		if (errno == EISDIR)
-		{
-			ft_putstr_fd("minishell: ", STDERR_FILENO);
-			ft_putstr_fd(str, STDERR_FILENO);
-			ft_putendl_fd(ERR_IS_DIR, STDERR_FILENO);
-		}
-		else if (errno == EACCES)
-		{
-			ft_putstr_fd("minishell: ", STDERR_FILENO);
-			ft_putstr_fd(str, STDERR_FILENO);
-			ft_putendl_fd(ERR_PERMISSION, STDERR_FILENO);
-		}
-		else if (errno == ENOENT)
-		{
-			ft_putstr_fd("minishell: ", STDERR_FILENO);
-			ft_putstr_fd(str, STDERR_FILENO);
-			ft_putendl_fd(ERR_NO_FILE_OR_DIR, STDERR_FILENO);
-		}
-		else if (errno == ENOTDIR)
-		{
-			ft_putstr_fd("minishell: ", STDERR_FILENO);
-			ft_putstr_fd(str, STDERR_FILENO);
-			ft_putendl_fd(ERR_NOT_DIR, STDERR_FILENO);
-		}
-		else
-		{
-			ft_putstr_fd("minishell: ", STDERR_FILENO);
-			ft_putstr_fd(str, STDERR_FILENO);
-			ft_putendl_fd(strerror(errno), STDERR_FILENO);
-		}
-		// set signal to errno
+		ms_errno_message(str, errno);
 	}
-	else if (msg) 
+	else if (msg)
 	{
-		ft_putstr_fd("(no errno) minishell: ", STDERR_FILENO);
+		ft_putstr_fd("minishell: ", STDERR_FILENO);
 		ft_putstr_fd(str, STDERR_FILENO);
 		ft_putendl_fd(msg, STDERR_FILENO);
-		// set signal to err_code
-	//	exit (err_code);
 	}
 	else
-	{
 		ft_putendl_fd(str, STDERR_FILENO);
-		// set signal to err_code
-	}
 	return (return_code);
 }
