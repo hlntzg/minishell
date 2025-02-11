f339f74dfb8ef9d68608279288eed040a9eecc3e
Helena Utzig <helenautzig@gmail.com>
2025-02-11 08:45:11 +0200

    norm OK for headers

--- includes/ms.h
+++ includes/ms.h
@@ -6,7 +6,7 @@
 /*   By: hutzig <hutzig@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 11:50:10 by hutzig            #+#    #+#             */
-/*   Updated: 2025/02/10 17:05:29 by hutzig           ###   ########.fr       */
+/*   Updated: 2025/02/11 08:34:51 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,12 +47,11 @@ void	free_ast(t_tree_node *ast);
 // closing
 void	close_heredoc_fds(t_tree_node *ast);
 void	close_fds(int read, int write);
+int		std_save(t_data *data);
+int		std_reset(t_data *data);
 
 //others
 int		blank_input(char *str);
-
-int	std_save(t_data *data);
-int	std_reset(t_data *data);
 
 // env
 void	set_environment(t_data *data, char **env);
@@ -60,9 +59,11 @@ char	*env_get_value(t_data *data, char *key);
 int		env_get_key(t_data *data, char *key);
 int		env_update_value(t_data *data, char *key, char *new_value);
 t_env	*env_get_node(t_data *data, char *key);
+
 // env utils
 char	**env_get_array_str(t_data *data);
 char	**exe_get_path(char **envp);
+
 // env utils lst
 t_env	*env_lstnew(char *key, char *value);
 t_env	*env_lstlast(t_env *lst);
@@ -73,15 +74,13 @@ int		env_lstsize(t_env *lst);
 // execution
 int		ms_pre_exe_newline(t_data *data);
 int		ms_execute_newline(t_data *data, int *status);
-
 char	*get_abs_path(char *cmd, char **path);
-
+int		ms_exe_command(t_data *data, char **_cmd, int *_pipe_fd);
 int		ms_exe_builtin_cmd(t_data *data, char **_cmd, int *_pipe_fd);
 int		ms_exe_external_cmd(t_data *data, char **_cmd, int *_pipe_fd);
 int		ms_handle_pipe_execution(t_data *data, t_tree_node *ast, int *_pipe_fd);
 int		ms_handle_redirection_execution(t_data *data,
 			t_tree_node *ast, int *_pipe_fd);
-int		ms_exe_command(t_data *data, char **_cmd, int *_pipe_fd);
 
 //heredoc
 int		ms_handle_heredoc(t_data *data, char *delimiter);
@@ -90,8 +89,8 @@ char	*process_character(char *expanded, char c, int *s_quote, int *d_quote);
 int		ms_open_file(t_data *data, t_tree_node *ast, t_tree_node *prev);
 int		ms_heredoc(t_data *data, t_tree_node *ast, char *delimiter);
 void	ms_exe_set_heredoc(t_data *data, t_tree_node *ast);
-
 void	ms_exe_heredoc(t_data *data, int _out, char *eof, int expansion);
+
 // builtins
 int		builtins(char *cmd);
 int		ms_builtin_execution(t_data *data, char **_cmd, int *_out);
@@ -104,9 +103,11 @@ int		ms_export(t_data *data, char **_cmd, int *_out);
 int		ms_pwd(t_data *data, char **_cmd, int *_out);
 int		ms_unset(t_data *data, char **_cmd);
 char	*ms_getpwd(t_data *data);
+
 // builtins utils
 int		count_cmd_args(char **_cmd);
 bool	valid_builtin_args(char *arg);
+
 // printing builtins
 void	builtins_print_env_variables(t_data *data, int fd);
 void	builtins_print_export_variables(t_data *data, int fd);
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: nmeintje <nmeintje@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 10:53:06 by nmeintje          #+#    #+#             */
-/*   Updated: 2025/02/09 14:56:25 by hutzig           ###   ########.fr       */
+/*   Updated: 2025/02/11 08:42:41 by hutzig           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,73 +17,73 @@
 
 extern int	g_sig;
 
-int     process_user_input(t_data *data, char *str, int *status);
+int			process_user_input(t_data *data, char *str, int *status);
 
 // signals
-void	set_signals(void);
-void	heredoc_signal(void);
-void	quit_heredoc(t_data *signal);
-void	set_heredoc_signal(int signum);
-void	handle_sigint(int sig);
-void	ignore_signals(void);
-void	child_signal(void);
-void	handle_sigquit(int signum);
-void	handle_sigint_exe(int signum);
-void	heredoc_sigint_exe(int signum);
-void    restore_main_signals(void);
+void		set_signals(void);
+void		heredoc_signal(void);
+void		quit_heredoc(t_data *signal);
+void		set_heredoc_signal(int signum);
+void		handle_sigint(int sig);
+void		ignore_signals(void);
+void		child_signal(void);
+void		handle_sigquit(int signum);
+void		handle_sigint_exe(int signum);
+void		heredoc_sigint_exe(int signum);
+void		restore_main_signals(void);
 
 // lexing
-t_token *tokenizer(t_data *data, char *str);
-t_token	*new_token(t_type type, char *content);
-void	add_tokens(t_token **token, t_token *new);
-char	*ft_strndup(char *src, int size);
-void 	free_tokens(t_token *token);
+t_token		*tokenizer(t_data *data, char *str);
+t_token		*new_token(t_type type, char *content);
+void		add_tokens(t_token **token, t_token *new);
+char		*ft_strndup(char *src, int size);
+void		free_tokens(t_token *token);
 
 // parsing
-t_tree_node *parse_tokens(t_token **tokens);
+t_tree_node	*parse_tokens(t_token **tokens);
 t_tree_node	*create_redirection(t_token **tokens, t_token *temp);
 t_tree_node	*new_tree_node(t_type type);
-t_tree_node *parse_redirection(t_token **tokens);
-t_tree_node *create_file_node(t_token *token);
+t_tree_node	*parse_redirection(t_token **tokens);
+t_tree_node	*create_file_node(t_token *token);
 int			argument_count(t_token *token);
-int has_space(const char *str);
-//void		free_ast(t_tree_node *node);
-int         count_expanded_args(t_token *tokens);
-int         expand_count(char *content);
-char	**split_expand(char **value, char *content, int *i);
+int			has_space(const char *str);
+int			count_expanded_args(t_token *tokens);
+int			expand_count(char *content);
+char		**split_expand(char **value, char *content, int *i);
 
 // expansion
-void    expand_variables(t_token **tokens, t_data *data);
-char    *get_variable_value(t_env *env, char *var_name);
-char    *get_variable_name(char *str, int start, int *length);
-char    *ft_strjoin_char(char *str, char c);
-char    *handle_exit_code(char *expanded, int exit_code, int *index);
-void	free_null_node(t_token **tok, t_token **cur, t_token **prev);
-char	*process_character(char *expanded, char c, int *s_quote, int *d_quote);
+void		expand_variables(t_token **tokens, t_data *data);
+char		*get_variable_value(t_env *env, char *var_name);
+char		*get_variable_name(char *str, int start, int *length);
+char		*ft_strjoin_char(char *str, char c);
+char		*handle_exit_code(char *expanded, int exit_code, int *index);
+void		free_null_node(t_token **tok, t_token **cur, t_token **prev);
+char		*process_character(char *expanded, char c,
+				int *s_quote, int *d_quote);
 
 // heredoc
-int     quoted_eof(char *delimiter);
-char	*update_eof(char *delimiter);
+int			quoted_eof(char *delimiter);
+char		*update_eof(char *delimiter);
 
 //validity
-bool	no_lexical_errors(char *str);
-void	quote_count(char *c, int *s_quote, int *d_quote);
-int		is_redirection(char *c);
+bool		no_lexical_errors(char *str);
+void		quote_count(char *c, int *s_quote, int *d_quote);
+int			is_redirection(char *c);
 
 // free and exit
-void	free_tree(t_tree_node *node);
-int		ft_error(char *str);
-void free_token(t_token **tokens);
+void		free_tree(t_tree_node *node);
+int			ft_error(char *str);
+void		free_token(t_token **tokens);
 
 // free
-void	free_char_double_ptr(char ***ptr);
-void	ms_free(t_data *data);
-void	free_array(char **array);
+void		free_char_double_ptr(char ***ptr);
+void		ms_free(t_data *data);
+void		free_array(char **array);
 
-// testing
-void print_tokens(t_token *token);
-void test_tokenizer(void);
-void test_parser(void);
-void print_ast(t_tree_node *node, int depth);
-
+/* testing
+void		print_tokens(t_token *token);
+void		test_tokenizer(void);
+void		test_parser(void);
+void		print_ast(t_tree_node *node, int depth);
+*/
 #endif
